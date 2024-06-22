5cff46fec8e61d44095f48314c9f67a9b0cc993d
Abhishek Peiris <abhisheklpeiris@gmail.com>
2024-06-22 23:01:54 +0530

    Register

---
+++ frontend/src/pages/auth/Register.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import styles from "./auth.module.scss"
+import Card from '../../components/card/Card';
+import { TiUserAddOutline } from 'react-icons/ti';
+import { Link } from 'react-router-dom';
+import PasswordInput from '../../components/passwordInput/PasswordInput';
+
+
+const initialState = {
+    name: "",
+    email: "",
+    password: "",
+    password2: "",
+}
+
+const Register = () => {
+    const [formData, setFormData] = useState(initialState);
+
+    const {name, email, password, password2} = formData
+
+    const handleInputChange = () => {
+
+    }
+
+    const loginUser = () => {
+
+    }
+
+    return (
+        <div className={`container ${styles.auth}`}>
+            <Card>
+                <div className={styles.form}>
+                    <div className="--flex-center">
+                        <TiUserAddOutline size={35} color='#999' />
+                    </div>
+                    <h2>Register</h2>                   
+
+                    <form onSubmit={loginUser}>
+                    <input type='text' placeholder='Name' required name="name" value={name} onChange={handleInputChange} />
+                        <input type='email' placeholder='Email' required name="email" value={email} onChange={handleInputChange} />
+                        <PasswordInput placeholder='Password' name="password" value={password} onChange={handleInputChange} />
+                        <PasswordInput placeholder='Confirm Password' name="password2" value={password2} onChange={handleInputChange} />
+                        <button type='submit' className='--btn --btn-primary --btn-block'>Register</button>
+                    </form>
+
+                    <span className={styles.register}>
+                        <Link to="/">Home</Link>
+                        <p> &nbsp; Already have an account? &nbsp; </p>
+                        <Link to="/login">Login</Link>
+                    </span>
+                </div>
+            </Card>
+        </div>
+    );
+}
+
+export default Register;
