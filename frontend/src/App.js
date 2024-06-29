faeda77f8cd1ea5a188bbdc06c5a5741aec86ba8
Abhishek Peiris <abhisheklpeiris@gmail.com>
2024-06-29 16:25:30 +0530

    reset password

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Layout from "./components/layout/Layout";
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
 import Forgot from "./pages/auth/Forgot";
+import Reset from "./pages/auth/Reset";
 function App() {
   return (
     <>
@@ -13,6 +14,7 @@ function App() {
           <Route path="/login" element={<Login />}/>
           <Route path="/register" element={<Register />}/>
           <Route path="/forgot" element={<Forgot />}/>
+          <Route path="/resetPassword/:resetToken" element={<Reset />}/>
         </Routes>
       </BrowserRouter>
     </> 
