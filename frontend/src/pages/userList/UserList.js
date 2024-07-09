ba3fa9d251a3cc5c66dbe3ec7024964022e6ccd7
Abhishek Peiris <abhisheklpeiris@gmail.com>
2024-07-09 18:03:01 +0530

    change role

--- frontend/src/pages/userList/UserList.js
+++ frontend/src/pages/userList/UserList.js
@@ -4,6 +4,7 @@ import PageMenu from '../../components/pageMenu/PageMenu'
 import UserStats from '../../components/userStats/UserStats'
 import Search from '../../components/search/Search'
 import { FaTrashAlt } from 'react-icons/fa'
+import ChangeRole from '../../components/changeRole/ChangeRole'
 
 function UserList() {
     return (
@@ -41,7 +42,7 @@ function UserList() {
                                 <td>abhisheklpeiris@gmail.com</td>
                                 <td>Admin</td>
                                 <td>
-                                    ChangeRole
+                                    <ChangeRole />
                                 </td>
                                 <td>
                                     <span>
