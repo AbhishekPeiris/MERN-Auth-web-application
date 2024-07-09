21abafe10b6b20ebbc1b2260c93868ed706d302e
Abhishek Peiris <abhisheklpeiris@gmail.com>
2024-07-09 16:26:01 +0530

    search

--- frontend/src/pages/userList/UserList.js
+++ frontend/src/pages/userList/UserList.js
@@ -2,6 +2,7 @@ import React from 'react'
 import "./UserList.scss"
 import PageMenu from '../../components/pageMenu/PageMenu'
 import UserStats from '../../components/userStats/UserStats'
+import Search from '../../components/search/Search'
 
 function UserList() {
   return (
@@ -9,6 +10,17 @@ function UserList() {
         <div className='container'>
             <PageMenu />
             <UserStats />
+
+            <div className='user-list'>
+                <div className='--flex-between'>
+                    <span>
+                        <h3>All Users</h3>
+                    </span>
+                    <span>
+                        <Search />
+                    </span>
+                </div>
+            </div>
         </div>
     </section>
   )
